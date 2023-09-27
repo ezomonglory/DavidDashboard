1dae645b7b5b92e4be085d964c39dbd536ef8795
Ezomon Glory <ezomonglory01@gmail.com>
2023-09-27 18:56:36 +0100

    fixed sidebar

--- src/pages/widget/SidebarWidget/SidebarWidget.js
+++ src/pages/widget/SidebarWidget/SidebarWidget.js
@@ -9,7 +9,7 @@ function SidebarWidget() {
 
     const [href, setHref] = useState("")
 
-    useEffect(()=> {
+    useEffect(() => {
         const href = window.location.href
         setHref(href)
         console.log(window.location.href)
@@ -30,7 +30,7 @@ function SidebarWidget() {
                 <ul className='flex flex-col space-y-[16px] '>
 
                     <Link to="/admins">
-                        <div className={`${href=== "http://localhost:3000/#/admins" || href==="http://localhost:3000/#/admins" ? "bg-[#E5F2FF] text-[#036]" : " text-[#808080] " } rounded-[2px]   flex items-center p-[8px] gap-[24px] `}>
+                        <div className={`${href === "https://digipass-amber.vercel.app/#/admins" || href === "https://digipass-amber.vercel.app/#/registerUser" ? "bg-[#E5F2FF] text-[#036]" : " text-[#808080] "} rounded-[2px]   flex items-center p-[8px] gap-[24px] `}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                                 <circle cx="9.99984" cy="4.99996" r="3.33333" stroke="#808080" stroke-width="1.5" />
                                 <circle cx="14.9998" cy="13.3333" r="3.33333" stroke="#808080" stroke-width="1.5" />
@@ -41,7 +41,7 @@ function SidebarWidget() {
                         </div>
                     </Link>
                     <Link to="/clock-in">
-                        <div className={`${href=== "http://localhost:3000/#/clock-in" ? "bg-[#E5F2FF] text-[#036]" : " text-[#808080] " } rounded-[2px]  flex items-center p-[8px] gap-[24px] `}>
+                        <div className={`${href === "https://digipass-amber.vercel.app/#/clock-in" ? "bg-[#E5F2FF] text-[#036]" : " text-[#808080] "} rounded-[2px]  flex items-center p-[8px] gap-[24px] `}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                                 <path d="M13.3333 3.33472C15.1459 3.34481 16.1274 3.42518 16.7678 4.0655C17.5 4.79773 17.5 5.97625 17.5 8.33327V13.3333C17.5 15.6903 17.5 16.8688 16.7678 17.601C16.0355 18.3333 14.857 18.3333 12.5 18.3333H7.5C5.14298 18.3333 3.96447 18.3333 3.23223 17.601C2.5 16.8688 2.5 15.6903 2.5 13.3333V8.33327C2.5 5.97625 2.5 4.79773 3.23223 4.0655C3.87255 3.42518 4.85414 3.34481 6.66667 3.33472" stroke="#808080" stroke-width="1.5" />
                                 <path d="M8.75 11.6667L14.1667 11.6667" stroke="#808080" stroke-width="1.5" stroke-linecap="round" />
@@ -56,18 +56,18 @@ function SidebarWidget() {
                         </div>
                     </Link>
 
-                    
-                        <div className="text-[#808080]  flex items-center p-[8px] gap-[24px] ">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                                <path d="M7.9165 11.6666C9.29722 11.6666 10.4165 12.7859 10.4165 14.1666C10.4165 15.5473 9.29722 16.6666 7.9165 16.6666C6.53579 16.6666 5.4165 15.5473 5.4165 14.1666C5.4165 12.7859 6.53579 11.6666 7.9165 11.6666Z" stroke="#808080" stroke-width="1.5" />
-                                <path d="M12.0835 3.33335C10.7028 3.33335 9.5835 4.45264 9.5835 5.83335C9.5835 7.21407 10.7028 8.33335 12.0835 8.33335C13.4642 8.33335 14.5835 7.21407 14.5835 5.83335C14.5835 4.45264 13.4642 3.33335 12.0835 3.33335Z" stroke="#808080" stroke-width="1.5" />
-                                <path d="M12.5 14.1321L18.3333 14.1321" stroke="#808080" stroke-width="1.5" stroke-linecap="round" />
-                                <path d="M7.5 5.79871L1.66667 5.79871" stroke="#808080" stroke-width="1.5" stroke-linecap="round" />
-                                <path d="M1.6665 14.1321L3.33317 14.1321" stroke="#808080" stroke-width="1.5" stroke-linecap="round" />
-                                <path d="M18.3335 5.79871L16.6668 5.79871" stroke="#808080" stroke-width="1.5" stroke-linecap="round" />
-                            </svg>
-                            <h1 className='textmed  '>Settings</h1>
-                        </div>                    
+
+                    <div className="text-[#808080]  flex items-center p-[8px] gap-[24px] ">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                            <path d="M7.9165 11.6666C9.29722 11.6666 10.4165 12.7859 10.4165 14.1666C10.4165 15.5473 9.29722 16.6666 7.9165 16.6666C6.53579 16.6666 5.4165 15.5473 5.4165 14.1666C5.4165 12.7859 6.53579 11.6666 7.9165 11.6666Z" stroke="#808080" stroke-width="1.5" />
+                            <path d="M12.0835 3.33335C10.7028 3.33335 9.5835 4.45264 9.5835 5.83335C9.5835 7.21407 10.7028 8.33335 12.0835 8.33335C13.4642 8.33335 14.5835 7.21407 14.5835 5.83335C14.5835 4.45264 13.4642 3.33335 12.0835 3.33335Z" stroke="#808080" stroke-width="1.5" />
+                            <path d="M12.5 14.1321L18.3333 14.1321" stroke="#808080" stroke-width="1.5" stroke-linecap="round" />
+                            <path d="M7.5 5.79871L1.66667 5.79871" stroke="#808080" stroke-width="1.5" stroke-linecap="round" />
+                            <path d="M1.6665 14.1321L3.33317 14.1321" stroke="#808080" stroke-width="1.5" stroke-linecap="round" />
+                            <path d="M18.3335 5.79871L16.6668 5.79871" stroke="#808080" stroke-width="1.5" stroke-linecap="round" />
+                        </svg>
+                        <h1 className='textmed  '>Settings</h1>
+                    </div>
                 </ul>
             </div>
 
